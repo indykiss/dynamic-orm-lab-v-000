64354440666aa6395d6337d16fec855b59349a0c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-02 21:25:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
   
-  def self.find_by(atttribute)
+  def self.find_by(attribute)
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0]} = '#{attribute.values[0]}'"
     DB[:conn].execute(sql)
   
